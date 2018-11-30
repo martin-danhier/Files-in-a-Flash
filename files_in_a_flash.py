93789cb892d5c0b81b2ecc3154adf73601667217
Martin Danhier <martin.danhier@student.unamur.be>
2018-11-30 12:36:49 +0100

    specifications 1

---
+++ files_in_a_flash.py
@@ -0,0 +1,36 @@
+def files_in_a_flash(path):
+    """ Sorts all the files in path/unsorted by theme.
+
+    Parameters
+    ----------
+    path : The path to the main folder that countains labels.txt, a subfolder unsorted (files to sort), a subfolder sorted (files to learn) (str)    """
+    pass
+
+def get_words(path):
+    """ Creates a list of all useful words in the given text file.
+
+    Parameters
+    ----------
+    path : the path to the .txt file to read (str)
+
+    Returns
+    -------
+    words : a list of all useful words in the text file
+    """
+    pass
+
+def get_frequences(path):
+    """"""
+    pass
+
+def check_differences(frequences):
+    """"""
+    pass
+
+def get_theme_prob(theme_frequences,list_words):
+    """"""
+    pass
+
+def check_accuracy(path):
+    """"""
+    pass
